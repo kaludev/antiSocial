69588a25102c2d5173f9679a158c18db05adcb80
nickola23 <nikolamilanovic2305@gmail.com>
2022-11-07 21:00:13 +0100

    changes

--- public/js/script.js
+++ public/js/script.js
@@ -3,52 +3,57 @@ const register = document.getElementById("register");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-
-register.onsubmit = async e =>{
-    e.preventDefault();
-    try{
-        const body ={
-            username: username.value,
-            email: email.value,
-            password: password.value
+if(register){
+    register.onsubmit = async e =>{
+        e.preventDefault();
+        try{
+            const body ={
+                username: username.value,
+                email: email.value,
+                password: password.value
+            }
+            const options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            }
+            const res = await fetch("/api/users/register", options)
+            console.log(res)
+            const json = await res.json();
+            console.log(json)
+            if(!json.ok) throw new Error(json.message);
+            return (window.location.href = "/")
         }
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
+        catch(error){
+            alert(error);
         }
-        console.log(options)
-        const res = await fetch("/api/users/register", options)
-        const json = await res.json();
-        if(!json.ok) throw new Error(json.message);
-        parent.location='index.html';
-    }
-    catch(error){
-        alert(error.message);
     }
 }
 
-login.onsubmit = async e =>{
-    e.preventDefault();
-    try{
-        const body ={
-            username: username.value,
-            password: password.value
+if(login){
+    login.onsubmit = async e =>{
+        e.preventDefault();
+        try{
+            const body ={
+                username: username.value,
+                password: password.value
+            }
+            const res = await fetch("/api/users/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            }) 
+            const json = await res.json();
+            console.log(json)
+            if(!json.ok) throw new Error(json.message);
+            return (window.location.href = "/")
         }
-        const res = await fetch("http://localhost:5000/api/users/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        })
-        const json = await res.json();
-        if(!json.ok) throw new Error(json.message);
-        parent.location='index.html';
-    }
-    catch(error){
-        alert(error.message);
-    }
+        catch(error){
+            alert(error);
+        }
+}
 }
