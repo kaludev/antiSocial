94d2c9d6b45e856ad34a8a11100ee7d2b70915eb
nickola23 <nikolamilanovic2305@gmail.com>
2022-11-08 19:14:53 +0100

    da

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,13 +1,27 @@
+var socket = io('http://localhost:5000');
 
- 
-  var socket = io('http://localhost:5000');
+// Connection succeeded
+socket.on('success', function(data) {
+  console.log(data.message);
+  console.log('user info: ' + data.user.id + ' username: '+ data.user.username);
+  socket.join(data.user.id)
+})
+socket.on('error', function(err) {
+  throw new Error(err);
+});
 
-  socket.on('error', function(err) {
-    throw new Error(err);
-  });
-  // Connection succeeded
-  socket.on('success', function(data) {
-    console.log(data.message);
-    console.log('user info: ' + data.user.id + ' username: '+ data.user.username);
-  })
- 
+const chatForm = document.getElementById(`chatForm`);
+
+socket.on(`message`, message => {
+  console.log(message);
+})
+
+//Message submit
+chatForm.addEventListener(`submit`, (e) => {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+
+  //Emit message to server
+  socket.emit(`chatMessage`, msg);
+})
