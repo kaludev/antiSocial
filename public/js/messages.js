07b6bf0235ce527c34f2313e1d1c9c4b4192f4e5
Kaludev <luka.markovic2017@gmail.com>
2022-11-08 19:49:12 +0100

    added messages

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,10 +1,11 @@
 var socket = io('http://localhost:5000');
+
 const target = `3b0d22d84586e0200c9c`;
 // Connection succeeded
-socket.on('success', function(data) {
+socket.on('success', async function(data) {
   console.log(data.message);
   console.log('user info: ' + data.user.id + ' username: '+ data.user.username);
-  socket.join(data.user.id)
+  socket.emit('join', data.user.id);
 })
 socket.on('error', function(err) {
   throw new Error(err);
@@ -12,9 +13,9 @@ socket.on('error', function(err) {
 
 const chatForm = document.getElementById(`chatForm`);
 
+
 //Message from server
-socket.on(`privateMessage`, target,message => {
-  console.log(message);
+socket.on(`privateMessage`, user,message => {
   outputMessage(message)
 })
 
@@ -25,5 +26,5 @@ chatForm.addEventListener(`submit`, (e) => {
   const msg = e.target.elements.msg.value;
 
   //Emit message to server
-  socket.emit(`privateMessage`, msg);
+  socket.emit(`privateMessage`, target,msg);
 })
