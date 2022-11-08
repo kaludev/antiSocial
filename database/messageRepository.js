91db68fa25a4c5f2e187bb225b5f87a13c73ade1
Kaludev <luka.markovic2017@gmail.com>
2022-11-08 21:30:44 +0100

    adding features

--- database/messageRepository.js
+++ database/messageRepository.js
@@ -4,7 +4,18 @@ const mysql = require('./connect');
 
 const insertMessage = async (source,target,msg) =>{
     const id = uid(20);
-   await mysql.query('INSERT INTO userMessages(id,userSourceId,userTargetId,message) VALUES (?,?,?,?)',
-    [id,source,target,msg]
+   const data = await mysql.query('INSERT INTO userMessages(id,userSourceId,userTargetId,message) VALUES (?,?,?,?)',
+    [   id,
+        source,
+        target,
+        msg
+    ]
    )
+   await mysql.end();
+
+    if (data.affectedRows === 0) throw new Error();
 }
+const getMessagesBeetween = async (source,target) =>{
+    const data = await mysql.query('SELECT * from')
+}
+module.exports = {insertMessage};
