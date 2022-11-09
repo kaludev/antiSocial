9a41e878c74e4e8e48efc37a95fd5dd702adf1f8
Kaludev <luka.markovic2017@gmail.com>
2022-11-09 21:06:47 +0100

    added features

--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {register,login,addFriend,acceptFriend} = require('../controllers/users')
+const {register,login,addFriend,acceptFriend,deleteFriend,getFriends} = require('../controllers/users')
 const auth = require('../middleware/auth')
 const router = express.Router();
 
@@ -7,4 +7,7 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/addfriend/:username').get(auth,addFriend);
 router.route('/acceptfriend/:username').get(auth,acceptFriend);
+router.route('/deletefriend/:username').get(auth,deleteFriend);
+router.route('/getFriends').get(auth,getFriends);
+
 module.exports = router
