618b31ea11a818f3f07351f1ed825fb767e4c0c8
Kaludev <luka.markovic2017@gmail.com>
2022-11-11 22:06:07 +0100

    v2

--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {register,login,showMe,upload,addFriend,acceptFriend,deleteFriend,getFriends,search} = require('../controllers/users')
+const {register,login,showMe,upload,profilePic,addFriend,acceptFriend,deleteFriend,getFriends,search} = require('../controllers/users')
 const auth = require('../middleware/auth');
 const photoParser = require('../middleware/photoParser');
 const router = express.Router();
@@ -8,6 +8,8 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/showme').get(auth,showMe);
 router.route('/upload').post(auth,photoParser,upload);
+router.route('/profilePic').get(auth,profilePic)
+router.route('/profilePic/:username').get(auth,profilePic)
 router.route('/addfriend/:username').get(auth,addFriend);
 router.route('/acceptfriend/:username').get(auth,acceptFriend);
 router.route('/deletefriend/:username').get(auth,deleteFriend);
