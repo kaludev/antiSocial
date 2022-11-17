fe0b68a7d7cedc4f255f0d8ea5397caaaf0300b6
Kaludev <luka.markovic2017@gmail.com>
2022-11-17 20:37:26 +0100

    fixed bugs

--- frontend-v2.0-react/src/pages/profile/profile.jsx
+++ frontend-v2.0-react/src/pages/profile/profile.jsx
@@ -2,63 +2,62 @@ import React,{useState} from "react";
 import {Link,useNavigate} from 'react-router-dom'
 import "./profileStyle.css"
 
-class Profile extends React.Component
-{
-    render(){
-          return {
-            <>
-            <nav>
-                <a href="/public/index.html">
-                    <img src="./images/logo.png" alt="" />
-                </a>
-                <div>
-                    <input type="text" id="search" placeholder="Search" />
-                    <ul id = "list"></ul>
-                </div>
-                <ul>
-                    <a href="/public/index.html">
-                        <li>
-                            </ion-icon><ion-icon name="home-outline"></ion-icon>
-                        </li>
-                    </a>
-                    <a href="">
-                        <li>
-                            <ion-icon name="person"></ion-icon>
-                        </li>
-                    </a>
-                        <li class="logout">
-                            <ion-icon name="log-out-outline"></ion-icon>
-                        </li>
-                </ul>
-            </nav>
-            <div class="mainProfile">
-                <div class="profile">
-                    <div class="profliePicDiv">
-                        <div class="profilePicMain">
-                            <div class="updateProfilePic">
-                                <input type="file" id="inputFile" />
-                                <label class="labelFile" for="inputFile"><ion-icon name="cloud-upload-outline"></ion-icon></label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="profileDescDiv">
-                        <div class="profileName">Nikola Milanovic</div>
-                        <div class="profileUsername"><span class="statistics">@nikola.miilanovic</span></div>
-                        <div class="profileData">
-                            <div class="profileChats"><span class="statistics">23</span> likes</div>
-                            <div class="profileFriends"><span class="statistics">117</span> friends</div>
-                        </div>
-                        <div class="profileData">
-                            <button class="addFriend">Edit profile</button>
+
+function Profile(){
+    return (
+    <>
+        <nav>
+            <Link to="/public/index.html">
+                <img src="./images/logo.png" alt="" />
+            </Link>
+            <div>
+                <input type="text" id="search" placeholder="Search" />
+                <ul id = "list"></ul>
+            </div>
+            <ul>
+                <Link to="/public/index.html">
+                    <li>
+                        <ion-icon name="home-outline"></ion-icon>
+                    </li>
+                </Link>
+                <Link to="">
+                    <li>
+                        <ion-icon name="person"></ion-icon>
+                    </li>
+                </Link>
+                    <li className="logout">
+                        <ion-icon name="log-out-outline"></ion-icon>
+                    </li>
+            </ul>
+        </nav>
+        <div className="mainProfile">
+            <div className="profile">
+                <div className="profliePicDiv">
+                    <div className="profilePicMain">
+                        <div className="updateProfilePic">
+                            <input type="file" id="inputFile" />
+                            <label className="labelFile" for="inputFile"><ion-icon name="cloud-upload-outline"></ion-icon></label>
                         </div>
                     </div>
                 </div>
-                <div class="profileContent"> 
+                <div className="profileDescDiv">
+                    <div className="profileName">Nikola Milanovic</div>
+                    <div className="profileUsername"><span className="statistics">@nikola.miilanovic</span></div>
+                    <div className="profileData">
+                        <div className="profileChats"><span className="statistics">23</span> likes</div>
+                        <div className="profileFriends"><span className="statistics">117</span> friends</div>
+                    </div>
+                    <div className="profileData">
+                        <button className="addFriend">Edit profile</button>
+                    </div>
                 </div>
             </div>
-            </>
-          };
-    }
+            <div className="profileContent"> 
+            </div>
+        </div>
+    </>
+    )
 }
+
 
 export default Profile;
