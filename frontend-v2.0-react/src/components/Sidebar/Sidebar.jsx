22a3462a1f42c4913a27e035401ae796ea46df16
Kaludev <luka.markovic2017@gmail.com>
2022-12-01 20:16:27 +0100

    added sidebar js

---
+++ frontend-v2.0-react/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { IonIcon } from "@ionic/react";
+import { Link } from "react-router-dom";
+import {AddOutline,chatbubbleOutline,logOutOutline,peopleOutline,personOutline} from "ionicons/icons";
+
+function Sidebar() {
+
+    return (
+        <div className="mainMenu">
+            <div>
+                <IonIcon icon={AddOutline}></IonIcon>
+            </div>
+            <div className="openChat">
+                <IonIcon icon={chatbubbleOutline}></IonIcon>
+            </div>
+            <div>
+                <IonIcon icon={peopleOutline}></IonIcon>
+            </div>
+            <Link to="/public/profile.html">
+                <div className="second">
+                    <IonIcon icon={personOutline}></IonIcon>
+                </div>
+            </Link>
+            <div className="logout second">
+                    <IonIcon icon={logOutOutline}></IonIcon>
+            </div>
+        </div>
+    )
+}
